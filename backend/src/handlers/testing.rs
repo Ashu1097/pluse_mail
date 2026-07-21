a4179af455aa7fb0ab95790507b591106315d2b3
Adi-474u <178244368+Adi-474u@users.noreply.github.com>
2026-07-22 02:02:44 +0530

    api work started

---
+++ backend/src/handlers/testing.rs
@@ -0,0 +1,3 @@
+pub async fn hi()->String{
+    "testing".to_string()
+}
--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,5 +1,7 @@
 mod databases;
+mod handlers;
 use tokio;
+use axum::{Router, routing::get};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -10,5 +12,10 @@ async fn main() -> anyhow::Result<()> {
     let chromaconfig = databases::config::load_chroma_env().expect("failed to load chroma env");
     let chromaconnection = databases::chroma::get_chroma_client(&chromaconfig).await?;
     print!("{:?}", chromaconnection);
+
+    let app = Router::new().route("/", get(handlers::testing::hi));
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+
     Ok(())
 }
